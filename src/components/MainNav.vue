6b43ed2d48615f4b93b287ed176b94e827303aa2
jnhck <s2jaheuc@uni-trier.de>
2022-07-31 01:08:25 +0200

    i should improve my commit msgs

--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -4,7 +4,7 @@
       <div
         class="flex flex-nowrap h-full px-8 mx-auto border-b border-solid border-brand-gray-1"
       >
-        <a href="/" class="flex items-center h-full text-xl">Hobo Careers</a>
+        <a href="/" class="flex items-center h-full text-xl">{{ company }}</a>
       </div>
     </div>
   </header>
@@ -13,5 +13,10 @@
 <script>
 export default {
   name: "MainNav",
+  data() {
+    return {
+      company: "Hobo Careers",
+    };
+  },
 };
 </script>
