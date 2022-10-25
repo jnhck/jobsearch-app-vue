6765997a1e80fcc4d75ba14748f254c144bea6cd
jnhck <s2jaheuc@uni-trier.de>
2022-10-25 14:06:49 +0200

    lesson 189

--- src/App.vue
+++ src/App.vue
@@ -6,8 +6,8 @@
 </template>
 
 <script>
-import MainNav from "@/components/MainNav.vue";
-import Hero from "@/components/Hero.vue";
+import MainNav from "@/components/navigation/MainNav.vue";
+import Hero from "@/components/job-search/Hero.vue";
 
 export default {
   name: "App",
