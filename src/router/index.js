a8dcf99538574382818a71e0ee49b74d3301f7d3
jnhck <s2jaheuc@uni-trier.de>
2022-10-26 16:45:52 +0200

    lesson 220

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,9 @@
 import { createRouter, createWebHashHistory } from "vue-router";
-import HomeView from "@/views/HomeView.vue";
-import JobResultsView from "@/views/JobResultsView.vue";
+const HomeView = () => import("@/views/HomeView.vue");
+const JobResultsView = () =>
+  import(/* webpackChunkName: "jobs" */ "@/views/JobResultsView.vue");
+const JobView = () =>
+  import(/* webpackChunkName: "jobs" */ "@/views/JobView.vue");
 
 const routes = [
   {
@@ -12,6 +15,11 @@ const routes = [
     path: "/jobs/results",
     name: "JobResults",
     component: JobResultsView,
+  },
+  {
+    path: "/jobs/results/:id",
+    name: "JobListing",
+    component: JobView,
   },
 ];
 
--- tests/unit/components/navigation/SubNav.test.js
+++ tests/unit/components/navigation/SubNav.test.js
@@ -2,42 +2,31 @@ import { mount } from "@vue/test-utils";
 import SubNav from "@/components/navigation/SubNav";
 
 describe("SubNav", () => {
+  const createConfig = (routeName) => ({
+    global: {
+      mocks: {
+        $route: {
+          name: routeName,
+        },
+      },
+      stubs: {
+        FontAwesomeIcon: true,
+      },
+    },
+  });
+
   describe("when user is on job page", () => {
     it("displays job count", () => {
-      const $route = {
-        name: "JobResults",
-      };
-
-      const wrapper = mount(SubNav, {
-        global: {
-          mocks: {
-            $route,
-          },
-          stubs: {
-            FontAwesomeIcon: true,
-          },
-        },
-      });
+      const routeName = "JobResults";
+      const wrapper = mount(SubNav, createConfig(routeName));
       const jobCount = wrapper.find("[data-test='job-count']");
       expect(jobCount.exists()).toBe(true);
     });
   });
   describe("when user is not on job page", () => {
     it("does _not_ display job count", () => {
-      const $route = {
-        name: "Home",
-      };
-
-      const wrapper = mount(SubNav, {
-        global: {
-          mocks: {
-            $route,
-          },
-          stubs: {
-            FontAwesomeIcon: true,
-          },
-        },
-      });
+      const routeName = "Home";
+      const wrapper = mount(SubNav, createConfig(routeName));
       const jobCount = wrapper.find("[data-test='job-count']");
       expect(jobCount.exists()).toBe(false);
     });
