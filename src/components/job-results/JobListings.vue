25ec23eed7b3af6fdad4bea411e27d2b3f67511e
jnhck <s2jaheuc@uni-trier.de>
2022-10-27 23:59:05 +0200

    lesson 229

---
+++ src/components/job-results/JobListings.vue
@@ -0,0 +1,20 @@
+<template>
+  <main class="flex-auto p-8 bg-brand-gray-2">
+    <ol>
+      <job-listing />
+      <job-listing />
+      <job-listing />
+    </ol>
+  </main>
+</template>
+
+<script>
+import JobListing from "@/components/job-results/JobListing.vue";
+
+export default {
+  name: "JobListings",
+  components: {
+    JobListing,
+  },
+};
+</script>
