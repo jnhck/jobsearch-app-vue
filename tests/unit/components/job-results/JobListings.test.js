10adeb316d02f1568064f13d17024046e424b342
jnhck <s2jaheuc@uni-trier.de>
2022-10-30 22:40:25 +0100

    lesson 256

--- tests/unit/components/job-results/JobListings.test.js
+++ tests/unit/components/job-results/JobListings.test.js
@@ -1,8 +1,38 @@
+import { shallowMount, flushPromises } from "@vue/test-utils";
 import axios from "axios";
+jest.mock("axios");
+
+import JobListings from "@/components/job-results/JobListings.vue";
 
 describe("JobListings", () => {
-  it("succeeds", () => {
-    console.log(axios);
-    expect("1").toBe("1");
+  const createRoute = (queryParams = {}) => ({
+    query: {
+      page: "5",
+      ...queryParams,
+    },
+  });
+
+  const createConfig = (route) => ({
+    global: {
+      mocks: {
+        $route: route,
+      },
+    },
+  });
+
+  it("fetches jobs", () => {
+    axios.get.mockResolvedValue({ data: [] });
+    const $route = createRoute();
+    shallowMount(JobListings, createConfig($route));
+    expect(axios.get).toHaveBeenCalledWith("http://localhost:3000/jobs");
+  });
+
+  it("creates a job listing for a maximum of 10 jobs", async () => {
+    axios.get.mockResolvedValue({ data: Array(15).fill({}) });
+    const $route = createRoute({ page: "1" });
+    const wrapper = shallowMount(JobListings, createConfig($route));
+    await flushPromises();
+    const jobListings = wrapper.findAll("[data-test='job-listing']");
+    expect(jobListings).toHaveLength(10);
   });
 });
